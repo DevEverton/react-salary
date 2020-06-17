583dfd5cc44650b9403766165ef83998b92ece8d
Everton Carneiro <Everton@MacBook-Pro-de-Everton.local>
2020-06-16 21:32:05 -0300

    Updates

---
+++ precache-manifest.7edcb9590d24df247e76138e9fe94260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88648746ad7f079856681a924cfa601a",
+    "url": "/react-salary/index.html"
+  },
+  {
+    "revision": "d890a53e963f969d56c4",
+    "url": "/react-salary/static/css/main.dd3076ef.chunk.css"
+  },
+  {
+    "revision": "47972a21242054dc2c4b",
+    "url": "/react-salary/static/js/2.273c867b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-salary/static/js/2.273c867b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d890a53e963f969d56c4",
+    "url": "/react-salary/static/js/main.c02ba039.chunk.js"
+  },
+  {
+    "revision": "ca8a3b54b8233161f714",
+    "url": "/react-salary/static/js/runtime-main.19960706.js"
+  }
+]);
